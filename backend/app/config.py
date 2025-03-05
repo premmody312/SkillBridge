cdf0037c8560622ed65561c3ff9542fdb9e41201
nath9777 <nath.atul1@gmail.com>
2025-03-05 13:21:13 -0800

    modifications

--- backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DATABASE_NAME = "skillbridge"
--- backend/app/database.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import MONGO_URI, DATABASE_NAME
-
-client = AsyncIOMotorClient(MONGO_URI)
-db = client[DATABASE_NAME]
