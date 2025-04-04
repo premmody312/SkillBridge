2920cf4a12fafb9e49f53137889b19da29b9cbf0
nath9777 <nath.atul1@gmail.com>
2025-04-04 12:15:13 -0700

    repo cleanup and README updated

--- backend old/routes/skillRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getSkillGap } = require('../controllers/skillController');
-const router = express.Router();
-
-router.get('/', getSkillGap);
-
-module.exports = router;
